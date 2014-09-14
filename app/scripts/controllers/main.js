a3e609722291f1ccdca7c9f4138bdbf8ee2afe33
Jim McGaw <jpmcgaw@gmail.com>
2014-09-14 09:57:51 -0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('hurbApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
